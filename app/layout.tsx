5f7830d9c958032136006d2f0786badc01eef3b4
igalarey <igalarey@gmail.com>
2024-10-12 18:50:36 +0200

    Añadido favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,35 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/components/theme-provider';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import { Toaster } from '@/components/ui/toaster';
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Imanol Galarza | Portfolio',
-  description: 'Personal portfolio',
-};
+	title: 'Imanol Galarza | Portfolio',
+	description: 'Personal portfolio',
+	icons: {
+		icon: '/favicon.png'
+	}
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          <Footer />
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<head>
+				<link rel='icon' href='/favicon.png' sizes='any' />
+			</head>
+			<body className={inter.className}>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+					<Header />
+					<main className='min-h-screen'>{children}</main>
+					<Footer />
+					<Toaster />
+				</ThemeProvider>
+			</body>
+		</html>
+	)
 }
